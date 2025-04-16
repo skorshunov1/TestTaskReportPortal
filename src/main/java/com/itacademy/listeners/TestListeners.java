467b4cbf4a22707eb4bb24d60083a234b1608bf8
Stanislav <st.korshunov88@gmail.com>
2025-04-16 15:03:45 +0300

    Add class TestListeners

---
+++ src/main/java/com/itacademy/listeners/TestListeners.java
@@ -0,0 +1,31 @@
+package com.itacademy.listeners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListeners implements ITestListener {
+    private static final Logger LOGGER = LogManager.getLogger(TestListeners.class);
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        LOGGER.info("#######################################################");
+        LOGGER.info("Test " + result.getMethod().getMethodName() + " started");
+        LOGGER.info("#######################################################");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        LOGGER.info("#######################################################");
+        LOGGER.info("Test " + result.getMethod().getMethodName() + " success");
+        LOGGER.info("#######################################################");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        LOGGER.info("#######################################################");
+        LOGGER.info("Test " + result.getMethod().getMethodName() + " failure");
+        LOGGER.info("#######################################################");
+    }
+}
